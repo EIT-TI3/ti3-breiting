77398573f626c8d7860aa30e3069b2e7f332d179
YannicBreiting <63100391+YannicBreiting@users.noreply.github.com>
2021-05-16 22:06:55 +0200

    updated P03

--- praktikum3/p03BreitingYannic.py
+++ praktikum3/p03BreitingYannic.py
@@ -7,8 +7,8 @@ Funktionen sind aufrufbare Objekte, die mit dem 'call operator' aufgerufen werde
 from v3_util import check_time
 
 
-def read_data(fn, val_name='value'):
-    with open('C:/Users/Yanni/Documents/GitHub/ti3/praktikum3/' + fn) as file:
+def read_data(data, val_name='value'):
+    with open(data) as file:
         text = file.readlines()
 
     table = {'date': list(), 'time': list(), val_name: list()}
@@ -20,7 +20,7 @@ def read_data(fn, val_name='value'):
 
 
 def stats(table, val_name):
-    values = [float(element) for element in table[val_name]]
+    values = [float(element) for element in table[val_name] if element is not None]
 
     return len(values), min(values), max(values), sum(values) / len(values)
 
@@ -33,23 +33,26 @@ def add_entry(table, d, t, val_name0, val_name1, val0, val1):
 
 
 def merge(data0, data1):
-    key1, key2 = list(data0.keys())[-1], list(data1.keys())[-1]
+    key1, key2 = tuple(data0.keys())[-1], tuple(data1.keys())[-1]
     table = {'date': list(), 'time': list(), key1: list(), key2: list()}
 
     curr_ndx0, curr_ndx1 = 0, 0
-    while curr_ndx0 < len(data0['date']) and curr_ndx1 < len(data1['date']):
+    while curr_ndx0 < len(data0['date']) or curr_ndx1 < len(data1['date']):
         position = check_time(data0, data1, curr_ndx0, curr_ndx1)
+
         if position == -1:
             date, time = data0['date'][curr_ndx0], data0['time'][curr_ndx0]
-            val0, val1 = data0[key1][curr_ndx0], None
+            val0, val1 = int(data0[key1][curr_ndx0]), None
             curr_ndx0 += 1
+
         elif position == 1:
             date, time = data1['date'][curr_ndx1], data1['time'][curr_ndx1]
-            val0, val1 = None, data1[key2][curr_ndx1]
+            val0, val1 = None, int(data1[key2][curr_ndx1])
             curr_ndx1 += 1
+
         elif position == 0:
             date, time = data1['date'][curr_ndx1], data1['time'][curr_ndx1]
-            val0, val1 = data0[key1][curr_ndx0], data1[key2][curr_ndx1]
+            val0, val1 = int(data0[key1][curr_ndx0]), int(data1[key2][curr_ndx1])
             curr_ndx0 += 1
             curr_ndx1 += 1
 
@@ -61,10 +64,28 @@ def merge(data0, data1):
     return table
 
 
+def print_table(table, number_space=7, entry_space=15):
+
+    output = 'index'.ljust(number_space)
+
+    for title in table.keys():
+        output += f'{title}'.ljust(entry_space)
+
+    output += "\n"
+
+    for idx in range(len(table['date'])):
+        output += f'{idx+1}'.ljust(number_space)
+        for row in table:
+            output += f"{table[row][idx]}".ljust(entry_space)
+        output += '\n'
+
+    print(output)
+
+
 if __name__ == '__main__':
     fn = "TESTcsv_803_172.csv"
     table_no2 = read_data(fn, "no2")
     fn = "TESTcsv_803_132G.csv"
     table_fs = read_data(fn, "fs")
     table_new = merge(table_no2, table_fs)
-    pass
+    print_table(table_new)
