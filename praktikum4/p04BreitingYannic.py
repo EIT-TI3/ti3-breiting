91a86b34a15ff7fa17a7a0e8e82d0f2af601ed66
YannicBreiting <63100391+YannicBreiting@users.noreply.github.com>
2021-05-28 16:33:15 +0200

    Update p04BreitingYannic.py

--- praktikum4/p04BreitingYannic.py
+++ praktikum4/p04BreitingYannic.py
@@ -3,5 +3,5 @@ from praktikum4.new_reader import Reader
 
 if __name__ == '__main__':
     reader = Reader()
-    g = reader.read('Karte_float.xml')
-    g.dijkstra('16527615', '363215')
+    g = reader.read('a.xml')
+    g.dijkstra('2958693613', '147042')
