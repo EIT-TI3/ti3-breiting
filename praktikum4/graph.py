f7e2401c68ff4130ba02c4101782a79944bd23f4
YannicBreiting <63100391+YannicBreiting@users.noreply.github.com>
2021-05-30 11:22:07 +0200

    Update graph.py

--- praktikum4/graph.py
+++ praktikum4/graph.py
@@ -1,3 +1,5 @@
+
+from algorhythms.listSorting import NodeHeap
 from node import Node
 from edge import Edge
 
@@ -6,16 +8,30 @@ class Graph:
     def __init__(self):
         self.nodes = {}
         self.edges = {}
-        self.queue = []
+        self.heap = NodeHeap()
+
+    def __str__(self):
+        output = 'Knoten:\n' \
+                 '-------\n'
+        for node in self.nodes.values():
+            output += str(node) + '\n'
+
+        output += '\nKanten:\n' \
+                  '-------\n'
+
+        for edge in self.edges.values():
+            output += str(edge) + '\n'
+
+        return output
 
     def new_node(self, name=None) -> Node:
         node = Node(name)
-        self.nodes.update({node.name: node})
+        self.nodes[node.name] = node
         return node
 
     def new_edge(self, name, weight) -> Edge:
         edge = Edge(name, weight)
-        self.edges.update({edge.name: edge})
+        self.edges[edge.name] = edge
         return edge
 
     def find_node(self, name) -> Node:
@@ -30,20 +46,6 @@ class Graph:
             output = self.edges[name]
         except KeyError:
             output = None
-        return output
-
-    def __str__(self):
-        output = 'Knoten:\n' \
-                 '-------\n'
-        for node in self.nodes.values():
-            output += str(node) + '\n'
-
-        output += '\nKanten:\n' \
-                  '-------\n'
-
-        for edge in self.edges.values():
-            output += str(edge) + '\n'
-
         return output
 
     def path_length(self, path_node_names):
@@ -67,56 +69,57 @@ class Graph:
 
     @staticmethod
     def find_edge_between(n1, n2, weight=True):
-        connecting_node = set(n1.neighbours()) & {n2}
-        if len(connecting_node) != 0:
-            for edge in n1.get_connects():
-                if edge.get_connect() is n2:
-                    return edge.weight if weight else edge
-
-    def _initialize_dijkstra(self, start_node):
-
-        for node in self.nodes.values():
-            node.distance = float('inf')
-            node.predecessor = None
-
-        self.queue = [*self.nodes.values()]
-        start_node.distance = 0
+        for edge in n1.get_connects():
+            if edge.get_connect() is n2:
+                return edge.weight if weight else edge
 
     def dijkstra(self, start_node, end_node):
         start_node, end_node = self.find_node(start_node), self.find_node(end_node)
 
         self._initialize_dijkstra(start_node)
 
-        while len(self.queue) > 0:
-            self.queue.sort()
-            node = self.queue.pop(0)
+        while len(self.heap) > 0:
+            node = self.heap.pop_task()
 
             for neighbour in node.neighbours():
-                self.update_distance(node, neighbour)
+                self._update_distance(node, neighbour)
 
             if end_node.predecessor is not None:
-                self.create_shortest_path(end_node)
+                self._create_shortest_path(end_node)
                 break
 
-    def update_distance(self, node, neighbour):
+    def _initialize_dijkstra(self, start_node):
+        for node in self.nodes.values():
+            node.distance = float('inf')
+            node.predecessor = None
+
+            if node is start_node:
+                start_node.distance = 0
+
+            self.heap.add_task(node, priority=node.distance)
+
+    def _update_distance(self, node, neighbour):
         new_distance = self.find_edge_between(node, neighbour) + node.distance
 
         if new_distance < neighbour.distance:
             neighbour.distance = new_distance
             neighbour.predecessor = node
+            self.heap.remove_task(neighbour)
+            self.heap.add_task(neighbour, priority=neighbour.distance)
 
-    def create_shortest_path(self, end_node):
+    def _create_shortest_path(self, end_node):
         predecessor = end_node
         path = 0
         way = [f'{end_node.name}\n']
 
-        while predecessor is not None:
-            if predecessor.predecessor is not None:
-                edge = self.find_edge_between(predecessor, predecessor.predecessor, weight=False)
-                way.append(f'{predecessor.predecessor.name} --> {edge.name}\n')
-                path += edge.weight
+        while predecessor.predecessor is not None:
+            edge = self.find_edge_between(predecessor, predecessor.predecessor, weight=False)
+            way.append(f'{predecessor.predecessor.name} --> {edge}\n')
 
+            path += edge.weight
             predecessor = predecessor.predecessor
 
         print(*reversed(way))
         print(f'Länge des Weges: {path}')
+
+
