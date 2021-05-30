6045e970a381e5469dd81da02c8aef51b8d705d5
YannicBreiting <63100391+YannicBreiting@users.noreply.github.com>
2021-05-30 11:22:12 +0200

    Update p04BreitingYannic.py

--- praktikum4/p04BreitingYannic.py
+++ praktikum4/p04BreitingYannic.py
@@ -4,4 +4,4 @@ from praktikum4.new_reader import Reader
 if __name__ == '__main__':
     reader = Reader()
     g = reader.read('a.xml')
-    g.dijkstra('2958693613', '147042')
+    g.dijkstra('1843938668', '1832448087')
