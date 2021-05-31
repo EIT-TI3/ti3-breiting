44959bb3d77b84167ded38ea26f06b781b4e2b55
YannicBreiting <63100391+YannicBreiting@users.noreply.github.com>
2021-05-31 22:41:21 +0200

    Update listSorting.py

--- algorhythms/listSorting.py
+++ algorhythms/listSorting.py
@@ -1,6 +1,5 @@
-import itertools
+import random
 from math import floor
-import heapq
 
 
 def slowsort(iterable, *_args):
@@ -20,93 +19,24 @@ def slowsort(iterable, *_args):
 
         slowsort(iterable, i, j - 1)
 
-    return
+
+def quicksort(iterable, lower, higher):
+    if lower < higher:
+        p = partition(iterable, lower, higher)
+        quicksort(iterable, lower, p-1)
+        quicksort(iterable, p + 1, higher)
 
 
-class BinaryTree(object):
-    root = None
+def partition(iterable, lower, higher):
+    pivot = iterable[higher]
+    i = lower
+    for j in range(lower, higher):
+        if iterable[j] < pivot:
+            iterable[i], iterable[j] = iterable[j], iterable[i]
 
-    @classmethod
-    def create_tree(cls, iterable):
-        iterable = iterable.copy()
-        cls.root = BinaryTreeNode(iterable.pop())
-
-        for element in range(len(iterable)):
-            cls.root.insert(iterable.pop(), cls.root)
-        return cls
-
-    def search_recursively(self, value, node):
-        if value is None or value is node:
-            return value
-        if value <= node.left:
-            return self.search_recursively(value, node.left)
-        if value > node.right:
-            return self.search_recursively(value, node.right)
+    iterable[i], iterable[higher] = iterable[higher], iterable[i]
+    return i
 
 
-class BinaryTreeNode(object):
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-    def __gt__(self, other):
-        return self.value > other
-
-    def __lt__(self, other):
-        return self.value < other
-
-    def __le__(self, other):
-        return self.value <= other
-
-    def __ge__(self, other):
-        return self.value >= other
-
-    @staticmethod
-    def insert(value, node):
-        if node.left is None:
-            node.left = BinaryTreeNode(value)
-
-        elif node.right is None:
-            node.right = BinaryTreeNode(value)
-
-        elif value < node.right:
-            return node.insert(value, node.left)
-
-        else:
-            return node.insert(value, node.right)
-
-
-class NodeHeap:
-    REMOVED = '<removed-task>'  # placeholder for a removed task
-    counter = itertools.count()  # unique sequence count
-
-    def __init__(self):
-        self.heap = []
-        self.entries = {}
-
-    def __len__(self):
-        return len(self.heap)
-
-    def add_task(self, task, priority=0):
-        'Add a new task or update the priority of an existing task'
-        if task in self.entries:
-            self.remove_task(task)
-        count = next(self.counter)
-        entry = [priority, count, task]
-        self.entries[task] = entry
-        heapq.heappush(self.heap, entry)
-
-    def remove_task(self, task):
-        'Mark an existing task as REMOVED.  Raise KeyError if not found.'
-        entry = self.entries.pop(task)
-        entry[-1] = self.REMOVED
-
-    def pop_task(self):
-        'Remove and return the lowest priority task. Raise KeyError if empty.'
-        while self.heap:
-            priority, count, task = heapq.heappop(self.heap)
-            if task is not self.REMOVED:
-                del self.entries[task]
-                return task
-        raise KeyError('pop from an empty priority queue')
+test_list = [random.randint(0, 10**3) for _ in range(10**3)]
+quicksort(test_list, 0, len(test_list)-1)
